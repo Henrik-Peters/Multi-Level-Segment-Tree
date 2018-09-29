31a5d37b96234c6a1f67ad271e64e8b828454361
Henrik-Peters <peters-henrik@outlook.de>
2018-09-29 12:52:40 +0200

    Initial project added

---
+++ src/main/scala/com/github/peters/henrik/segmenttree/App.scala
@@ -0,0 +1,16 @@
+// ---------------------------------------------------------------------
+// MIT License
+// Copyright (c) 2018 Henrik Peters
+// See LICENSE file in the project root for full license information.
+// ---------------------------------------------------------------------
+package com.github.peters.henrik.segmenttree
+
+/**
+  * @author Henrik Peters
+  */
+object App {
+
+  def main(args : Array[String]) {
+    println("Multi-Level Segment Tree")
+  }
+}
